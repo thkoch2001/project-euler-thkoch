e479e6d8d3603345ab6ab4afe27978047afd867b
Thomas Koch <thomas@koch.ro>
2014-06-24 21:22:14 +0200

    new Version of euler 3 that takes only seconds

--- euler0003.hs
+++ euler0003.hs
@@ -2,12 +2,27 @@
 isDivisor :: Integer -> Integer -> Bool
 isDivisor n d = (n `mod` d) == 0
 
-isPrime :: Integer -> Bool
-isPrime n = n `elem` [2, 3, 5, 7, 11, 13]
-            || (not $ any (isDivisor n) $ 2:[3, 5..(n `div` 2)])
+findPrimeLarger :: Integer -> [Integer] -> Integer
+findPrimeLarger n primes =
+  if isPrime
+    then n
+    else findPrimeLarger (n + 2) primes
+  where
+    testPrimes = dropWhile (\x -> 2 * x > n) primes
+    isPrime = not $ any (isDivisor n) testPrimes
 
-primeFactors :: Integer -> [Integer]
-primeFactors n = [x | x <- 2:[3,5..(n `div` 2)], (isPrime x) && (isDivisor n x)]
+nextPrime :: [Integer] -> [Integer]
+nextPrime [2] = [3, 2]
+nextPrime primes@(x:xs) = (findPrimeLarger (x + 2) primes) : primes
+nextPrime _ = undefined
+
+reduceWithPrimes :: Integer -> [Integer] -> Integer
+reduceWithPrimes n primes@(x:xs)
+  | n < x         = undefined
+  | n == x        = n
+  | isDivisor n x = reduceWithPrimes (n `div` x) primes
+  | otherwise     = reduceWithPrimes n (nextPrime primes)
 
 euler0003 :: Integer -> Integer
-euler0003 n = maximum $ primeFactors n
+euler0003 n = reduceWithPrimes n [2]
+
